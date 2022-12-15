4f4ceb39e88fdd680d7679b352a30935037d7c90
justinkenramoso <justinken.ramoso@gmail.com>
2022-12-15 16:27:39 +0800

    Remade game page entirely.

--- src/components/pages/Game.jsx
+++ src/components/pages/Game.jsx
@@ -4,12 +4,15 @@ import Nav from "../Nav";
 import Footer from "../Footer";
 import { useState, useEffect } from "react";
 import axios from "axios";
+import $ from "jquery";
+import * as DOMPurify from "dompurify";
 
 function Game() {
   const { id } = useParams();
   const [game, setGame] = useState(null);
   const [bg, setBg] = useState("");
   const [loading, setLoading] = useState(true);
+  const [desc, setDesc] = useState("No description.");
 
   // Get Games List
   const options = {
@@ -32,6 +35,8 @@ function Game() {
         } else {
           setBg(`url(${response.data.thumbnail})`);
         }
+        setDesc(response.data.description);
+
         setLoading(false);
       })
       .catch(function (error) {
@@ -40,18 +45,17 @@ function Game() {
   }, []);
 
   console.log(game);
+  $("#desc-div").html(desc);
 
   return (
     <div>
       <Nav nav={["", "active-nav", "", ""]} />
       {!loading && (
         <section id="game" style={{ backgroundImage: bg }}>
-          <div className="container pt-5">
-            <div className="row">
-              <div className="col-lg-6 px-5 px-lg-0">
-                <h1 className="slanted sl fw-bold text-center w-100">
-                  {game.title}
-                </h1>
+          <div className="container py-5">
+            <h1 className="slanted sl fw-bold text-center">{game.title}</h1>
+            <div className="row mt-5">
+              <div className="col-lg-6">
                 <div className="row mx-auto py-3">
                   <div className="col-6">
                     <p className="m-0 p-0">Genre</p>
@@ -69,14 +73,18 @@ function Game() {
                     <p className="m-0 p-0">Developer</p>
                     <h5 className="fw-bold color1">{game.developer}</h5>
                   </div>
+                  <div className="col-12">
+                    <p className="mt-3 p-0">
+                      Release Date:{" "}
+                      <span className="fw-bold color1 fs-5">
+                        {game.release_date}
+                      </span>
+                    </p>
+                  </div>
                 </div>
-                <p className="d-none d-lg-block lead p-2">
-                  {game.short_description}
-                </p>
-                <p className="d-block d-lg-none">{game.short_description}</p>
               </div>
-              <div className="col-lg-6 px-3 px-lg-5">
-                <div className=" d-flex justify-content-center align-items-center">
+              <div className="col-lg-6">
+                <div className="d-flex flex-column justify-content-center align-items-center h-100">
                   <div
                     className="row g-0 play-game rounded"
                     style={{ cursor: "pointer" }}
@@ -92,73 +100,134 @@ function Game() {
                         style={{ maxWidth: "300px" }}
                       />
                     </div>
-                    <div className="col-2 bg-black d-flex justify-content-center align-items-center rounded-end">
-                      <i className="fa-solid fa-gamepad fa-2xl"></i>
+                    <div className="col-2 bg-black d-flex flex-column justify-content-center align-items-center rounded-end">
+                      <h5 className="pb-2 fw-bold">PLAY</h5>
+                      <i className="m-0 fa-solid fa-gamepad fa-2xl"></i>
                     </div>
                   </div>
                 </div>
-                {game.screenshots.length < 3 && (
-                  <h5 className="mt-5 text-center">
-                    No screenshots available.
-                  </h5>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-6 px-3 px-lg-5"></div>
+        </section>
+      )}
+      {!loading && (
+        <section id="game-fulldesc">
+          <div className="container my-3 mb-5 px-4 px-lg-5">
+            <div className="row">
+              <div className="col-lg-6">
+                <h3 className="fw-bold">Game Description</h3>
+                <hr />
+                <div
+                  className="lead"
+                  dangerouslySetInnerHTML={{
+                    __html: DOMPurify.sanitize(desc),
+                  }}
+                ></div>
+              </div>
+              <div className="col-lg-6 d-flex justify-content-center align-items-center p-5">
+                {/* System Req */}
+                {!game.hasOwnProperty("minimum_system_requirements") && (
+                  <div className="mt-3">
+                    <h5>No system requirements specified.</h5>
+                  </div>
                 )}
-                {/* Carousel */}
-                {game.screenshots.length >= 3 && (
-                  <div
-                    id="carouselExampleControls"
-                    className="carousel slide mt-5 mb-5 mb-lg-0 border border-1"
-                    data-bs-ride="carousel"
-                  >
-                    <div className="carousel-inner">
-                      <div className="carousel-item active">
-                        <img
-                          src={game.screenshots[0].image}
-                          className="d-block w-100"
-                          alt="..."
-                        />
-                      </div>
-                      <div className="carousel-item">
-                        <img
-                          src={game.screenshots[1].image}
-                          className="d-block w-100"
-                          alt="..."
-                        />
-                      </div>
-                      <div className="carousel-item">
-                        <img
-                          src={game.screenshots[2].image}
-                          className="d-block w-100"
-                          alt="..."
-                        />
-                      </div>
-                    </div>
-                    <button
-                      className="carousel-control-prev"
-                      type="button"
-                      data-bs-target="#carouselExampleControls"
-                      data-bs-slide="prev"
-                    >
-                      <span
-                        className="carousel-control-prev-icon"
-                        aria-hidden="true"
-                      ></span>
-                      <span className="visually-hidden">Previous</span>
-                    </button>
-                    <button
-                      className="carousel-control-next"
-                      type="button"
-                      data-bs-target="#carouselExampleControls"
-                      data-bs-slide="next"
-                    >
-                      <span
-                        className="carousel-control-next-icon"
-                        aria-hidden="true"
-                      ></span>
-                      <span className="visually-hidden">Next</span>
-                    </button>
+                {game.hasOwnProperty("minimum_system_requirements") && (
+                  <div className="mt-3">
+                    <h5 className="mb-3">Minimum System Requirements:</h5>
+                    <table className="table table-dark table-striped-columns table-hover text-center">
+                      <tbody>
+                        <tr>
+                          <td>Operating System</td>
+                          <td>{game.minimum_system_requirements.os}</td>
+                        </tr>
+                        <tr>
+                          <td>Processor</td>
+                          <td>{game.minimum_system_requirements.processor}</td>
+                        </tr>
+                        <tr>
+                          <td>Graphics</td>
+                          <td>{game.minimum_system_requirements.graphics}</td>
+                        </tr>
+                        <tr>
+                          <td>Memory</td>
+                          <td>{game.minimum_system_requirements.memory}</td>
+                        </tr>
+                        <tr>
+                          <td>Storage</td>
+                          <td>{game.minimum_system_requirements.storage}</td>
+                        </tr>
+                      </tbody>
+                    </table>
                   </div>
                 )}
               </div>
+            </div>
+
+            <hr />
+            <h4 className="text-center">Gameplay Screenshots</h4>
+            <div className="d-flex justify-content-center">
+              {game.screenshots.length < 3 && (
+                <h5 className="mt-5 text-center">No screenshots available.</h5>
+              )}
+              {/* Carousel */}
+              {game.screenshots.length >= 3 && (
+                <div
+                  id="carouselExampleControls"
+                  className="carousel slide mb-5 mb-lg-0 border border-1"
+                  data-bs-ride="carousel"
+                  style={{ maxWidth: "700px" }}
+                >
+                  <div className="carousel-inner">
+                    <div className="carousel-item active">
+                      <img
+                        src={game.screenshots[0].image}
+                        className="d-block w-100"
+                        alt="..."
+                      />
+                    </div>
+                    <div className="carousel-item">
+                      <img
+                        src={game.screenshots[1].image}
+                        className="d-block w-100"
+                        alt="..."
+                      />
+                    </div>
+                    <div className="carousel-item">
+                      <img
+                        src={game.screenshots[2].image}
+                        className="d-block w-100"
+                        alt="..."
+                      />
+                    </div>
+                  </div>
+                  <button
+                    className="carousel-control-prev"
+                    type="button"
+                    data-bs-target="#carouselExampleControls"
+                    data-bs-slide="prev"
+                  >
+                    <span
+                      className="carousel-control-prev-icon"
+                      aria-hidden="true"
+                    ></span>
+                    <span className="visually-hidden">Previous</span>
+                  </button>
+                  <button
+                    className="carousel-control-next"
+                    type="button"
+                    data-bs-target="#carouselExampleControls"
+                    data-bs-slide="next"
+                  >
+                    <span
+                      className="carousel-control-next-icon"
+                      aria-hidden="true"
+                    ></span>
+                    <span className="visually-hidden">Next</span>
+                  </button>
+                </div>
+              )}
             </div>
           </div>
         </section>
